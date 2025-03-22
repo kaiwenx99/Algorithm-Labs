880bc7cd39917409d671d2418c16f300510c57e1
KaiwenX <kxiao12@my.bcit.ca>
2025-03-22 16:26:20 -0700

    Add wk11 lab

---
+++ wk11.py
@@ -0,0 +1,23 @@
+def bubble_sort(seq):
+    for j in range(len(seq) - 1, 0, -1):
+        for i in range(0, j):
+            if (i < j and seq[j] < seq[i]) or (i > j and seq[j] > seq[i]):
+                seq[i], seq[j] = seq[j], seq[i]
+
+def selection_sort(seq):
+    n = len(seq)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if seq[j] < seq[min_index]:
+                min_index = j
+        seq[i], seq[min_index] = seq[min_index], seq[i]
+
+def insertion_sort(seq):
+    for i in range(1, len(seq)):
+        key = seq[i]
+        j = i - 1
+        while j >= 0 and seq[j] > key:
+            seq[j + 1] = seq[j]
+            j -= 1
+        seq[j + 1] = key
